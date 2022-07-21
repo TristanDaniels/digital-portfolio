e3cd757a9c095d7e33226b19af2483d2f8e999f6
TristanDaniels <tristandaniels07@gmail.com>
2022-07-21 15:41:23 +0200

    update

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -141,6 +141,9 @@ h2 {
   align-items: center;
   flex-wrap: wrap;
 }
+.template:hover {
+  transform: scale(1.02);
+}
 .project-container {
   display: flex;
   flex-direction: row;
@@ -151,6 +154,7 @@ h2 {
   padding-top: 20px;
   padding-left: 20px;
   padding-right: 20px;
+  transition: all 0.5s ease;
 }
 .template img {
   width: 350px;
